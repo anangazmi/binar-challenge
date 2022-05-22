7334bc869e0dfb04aab80fae7120acb8f95e3625
Anang AZMI <azmiho9977@gmail.com>
2022-05-22 23:39:04 +0800

    inhenritance player

--- src/script/script.js
+++ src/script/script.js
@@ -11,3 +11,18 @@ const Computer = (Base) =>
   class extends Base {
     randomPick = (max) => Math.floor(Math.random() * max);
   };
+
+// Inheritance
+class Player_1 extends Player {
+  constructor(batu, kertas, gunting) {
+    super(batu, kertas, gunting);
+    this.#initiation();
+  }
+
+  // Encapsulation
+  #initiation() {
+    this.batu[0].id = "batu-player";
+    this.kertas[0].id = "kertas-player";
+    this.gunting[0].id = "gunting-player";
+  }
+}
